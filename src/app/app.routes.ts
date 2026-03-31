8ef1d09ed6ed2ce62c65297be76fb50cf8504e24
unknown <miguelbaltaz4r@gmail.com>
2026-03-31 01:38:20 -0600

    Pantalla en la que inicia

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,6 @@ import { WebPagesComponent } from './pages/web-pages/web-pages.component';
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'web-pages', component: WebPagesComponent },
-  { path: '', redirectTo: 'web-pages', pathMatch: 'full' },
-  { path: '**', redirectTo: 'web-pages' }
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'home' }
 ];
