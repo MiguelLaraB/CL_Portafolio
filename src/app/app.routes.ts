b2ab4670a09cb3a9a10a74d3962639b2aecc4857
unknown <miguelbaltaz4r@gmail.com>
2026-03-31 01:30:27 -0600

    home corregido

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,8 @@ import { HomeComponent } from './pages/home/home.component';
 import { WebPagesComponent } from './pages/web-pages/web-pages.component';
 
 export const routes: Routes = [
-  { path: 'web-pages', component: WebPagesComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'web-pages', component: WebPagesComponent },
   { path: '', redirectTo: 'web-pages', pathMatch: 'full' },
   { path: '**', redirectTo: 'web-pages' }
 ];
